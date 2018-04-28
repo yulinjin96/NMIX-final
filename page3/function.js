4afd97d7429784030b09c9dff47f3e9dd8fb8430
Sheng Chen <sheng0501@gmail.com>
2018-04-28 19:14:25 -0400

    add music

--- page3/function.js
+++ page3/function.js
@@ -20,10 +20,17 @@ function dragit(answer) {
 }
 
 function dropit(feedback_id, response) {
+    var correct_audio = document.createElement('audio');
+    correct_audio.setAttribute('src', 'https://www.soundjay.com/misc/sounds/bell-ring-01.mp3');
+    var wrong_audio = document.createElement('audio');
+    wrong_audio.setAttribute('src', 'https://www.soundjay.com/misc/sounds/fail-buzzer-01.mp3');
+
     $('#test').html(sumthing);
     if (response === sumthing) {
+        correct_audio.play();
         $('#feedback'+feedback_id).attr("src", "./images/right.png");
     } else {
+        wrong_audio.play();
         $('#feedback'+feedback_id).attr("src", "./images/wrong.jpeg");
     }
 }
